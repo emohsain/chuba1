3bb022722d93b377da34aaec80e3e351b40d44d8
emohsain <emohsain@gmail.com>
2021-09-03 15:40:58 -0700

    added devise

--- Gemfile
+++ Gemfile
@@ -51,3 +51,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#added gem devise to help with sign up,sign-in and sign out
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :views
+  devise_for :users
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # defined root_url to *something*
+  root to: "blogs#index"
 end
+
